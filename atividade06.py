468622e4ab72d5a1a80309bae94a7dec43478f50
Júlio César <140417209+juju-png@users.noreply.github.com>
2023-10-23 14:08:35 -0300

    Add files via upload

---
+++ atividade06.py
@@ -0,0 +1,21 @@
+# Digite um numero interio e veja sua tabuada
+input = float("digite um numero inteiro")
+print = float("numero inteiro:5")
+tabuada = "numero 5"
+tabuada = float ("tabuada")
+
+print(f"""
+      {t1}
+      {t1}
+      {t1}
+      {t1}
+      {t1}
+      {t1}
+      {t1}
+      {t1}
+      {t1}
+      
+      
+      """)
+
+
